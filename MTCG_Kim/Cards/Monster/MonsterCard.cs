fc5eba82d3a319133ff41d153dfabaa08201b692
Minu Kim <minu.kim2003@gmail.com>
2023-10-28 00:12:19 +0200

    creating base .cs files

---
+++ MTCG_Kim/Cards/Monster/MonsterCard.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MTCG_Kim.Cards.Monster
+{
+    internal abstract class MonsterCard
+    {
+    }
+}
---
+++ MTCG_Kim/Database/DatabaseHandler.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MTCG_Kim.Database
+{
+    internal class DatabaseHandler
+    {
+    }
+}
